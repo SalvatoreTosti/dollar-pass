375df190eae603956789d453eb1c97f24e906f08
Salvatore Tosti <saltosti@gmail.com>
2017-07-09 19:41:56 -0700

    Added basic simulation capabilities
    Users can simulate games featuring an arbitrary number of players which are initially distributed an arbitrary number of coins.
    These games can then be simulated to any number of steps into gameplay.

--- src/dollar_pass/core.clj
+++ src/dollar_pass/core.clj
@@ -1,7 +1,56 @@
 (ns dollar-pass.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn rand-nth-keys
+  [coll]
+  (rand-nth (keys coll)))
+
+(defn rand-nth-values
+  [coll]
+  (coll (rand-nth-keys coll)))
+
+(defn non-zero-dec
+  [curr-key mp]
+  (if (not (pos? (curr-key mp)))
+    mp
+    (update-in mp [curr-key] dec)))
+
+(defn key-step
+  [curr-key mp]
+  (let [rand-key (rand-nth-keys mp)]
+    (if (= rand-key curr-key)
+      (key-step curr-key mp)
+      (-> (non-zero-dec curr-key mp)
+          (update-in [rand-key] inc)))))
+
+(defn single-step-rec
+  [key-list mp]
+  (if (empty? key-list) mp
+      (->> (key-step (first key-list) mp)
+           (single-step-rec (rest key-list)))))
+
+(defn single-step
+  [mp]
+  (single-step-rec (keys mp) mp))
+
+(defn step
+  [number coll]
+  (loop [n number cl coll]
+  (if
+    (not (pos? n)) cl
+    (recur (dec n) (single-step cl)))))
+
+(defn starting-players
+  [number dollars]
+  (zipmap (map keyword
+               (map str (range 1 number)))
+          (take number
+                (repeat dollars))))
+
+(defn play
+  [player-count starting-amount steps]
+  (->>
+   (starting-players player-count starting-amount)
+   (step steps )))
+
+;(play 50 45 5000)
